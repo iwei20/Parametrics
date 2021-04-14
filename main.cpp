448e4ccd019b775785a5fbe344bb8e5fa1c9217b
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-04-14 00:16:02 -0400

    Modify main to also accept width height

--- main.cpp
+++ main.cpp
@@ -4,16 +4,17 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    if(argc <= 0) {
-        std::cout << "Usage: "
+    if(argc != 4) {
+        std::cout << "Usage: ./main.out [scriptname] [width] [height]\n";
+    } else {
+        std::ifstream fin(argv[1]);
+        if(fin.fail()) {
+            std::cout << "Could not read file\n";
+        } else {
+            screen s(std::stoi(argv[2]), std::stoi(argv[3]));
+            parser p;
+            p.parse(s, fin);
+            fin.close();
+        }
     }
-    std::ifstream fin(argv[1]);
-    if(fin.fail()) {
-        std::cout << "Could not read file\n";
-        return 0;
-    }
-    screen s(500, 500);
-    parser p;
-    p.parse(s, fin);
-    fin.close();
 }
